4ad2dfd5012e1aec20b871013daab9781b1f82eb
Rossajt <39495648+Rossajt@users.noreply.github.com>
2018-05-21 20:28:03 +0100

    Checkout Stage 1
    The first stage of the Checkout Problem

---
+++ checkout.java
@@ -0,0 +1,56 @@
+// Created by Ross Taylor
+import java.util.Scanner;
+public class checkout{
+
+    public static void main(String[] args)throws Exception{
+    	checkout program = new checkout();
+		String[] list = program.scanValues();
+		int price = program.calculatePrice(list);
+		program.printPrice(price);
+    }
+	
+    /**
+     * Method to scan values entered and places value into an array.
+     * @return
+     */
+    private String[] scanValues(){
+    	Scanner scan = new Scanner(System.in);
+		System.out.println("Please input the items you are buying.");
+		String purchase = scan.nextLine();
+		String[] list = purchase.trim().split("\\s+");
+		scan.close();
+		return list;
+    }
+    
+	/**
+	 * Method that calculates the price of apples and oranges in total.
+	 */
+    private int calculatePrice(String[] list){
+		String apple = "apple";
+		String orange = "orange";
+		int numberOfApples = 0;
+		int numberOfOranges = 0;
+		int price = 0;
+		for(int i = 0; i < list.length; i++){
+			if(list[i].toLowerCase().equals(apple)){
+				numberOfApples++;
+			}
+			else if(list[i].toLowerCase().equals(orange)){
+				numberOfOranges++;
+			}
+		}
+		price = (numberOfApples * 60) + (numberOfOranges * 25);
+		return price;
+    }
+    
+    /**
+     * Method responsible for printing the final output price.
+     * @param price
+     */
+    private void printPrice(int price){
+    	int pricePounds = price / 100;
+		int pricePence = price % 100;
+		System.out.println(String.format("Total Cost: £%d.%02d", pricePounds, pricePence));
+	}
+	
+}
